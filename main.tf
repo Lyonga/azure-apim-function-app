4a22ab27333bf6c984c17f119a4826f4118a39d5
clyonga <charles.lyonga1@accesscfa.com>
2026-02-23 16:05:09 -0500

    new

--- main.tf
+++ main.tf
@@ -217,7 +217,8 @@ resource "azurerm_key_vault" "kv_example" {
 resource "azurerm_key_vault_access_policy" "pipeline" {
   key_vault_id = azurerm_key_vault.kv_example.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+  //object_id    = data.azurerm_client_config.current.object_id
+  object_id    = azurerm_api_management.demo-charsett.identity[0].principal_id
 
   secret_permissions = [
     "Get", "List", "Set", "Delete", "Purge", "Recover"
@@ -237,15 +238,5 @@ resource "azurerm_key_vault_access_policy" "apim" {
 
   secret_permissions = [
     "Get"
-  ]
-}
-
-resource "azurerm_key_vault_access_policy" "pipeline" {
-  key_vault_id = azurerm_key_vault.kv_example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  secret_permissions = [
-    "Get", "List", "Set", "Delete", "Recover", "Purge"
   ]
 }
