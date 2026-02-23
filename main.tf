485b5b627ce120db8d91ac0335fc0e54b43b3da1
clyonga <charles.lyonga1@accesscfa.com>
2026-02-23 15:53:55 -0500

    not sure now

--- main.tf
+++ main.tf
@@ -239,3 +239,13 @@ resource "azurerm_key_vault_access_policy" "apim" {
     "Get"
   ]
 }
+
+resource "azurerm_key_vault_access_policy" "pipeline" {
+  key_vault_id = azurerm_key_vault.kv_example.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get", "List", "Set", "Delete", "Recover", "Purge"
+  ]
+}
