be56cfa352a6a849da7126ca1b6583c169871ff1
clyonga <charles.lyonga1@accesscfa.com>
2026-02-23 16:28:54 -0500

    tests agaionst main branch

--- main.tf
+++ main.tf
@@ -217,18 +217,26 @@ resource "azurerm_key_vault" "kv_example" {
 resource "azurerm_key_vault_access_policy" "pipeline" {
   key_vault_id = azurerm_key_vault.kv_example.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  //object_id    = data.azurerm_client_config.current.object_id
-  object_id    = azurerm_api_management.demo-charsett.identity[0].principal_id
+  object_id    = data.azurerm_client_config.current.object_id
+  //object_id    = azurerm_api_management.demo-charsett.identity[0].principal_id
 
   secret_permissions = [
     "Get", "List", "Set", "Delete", "Purge", "Recover"
   ]
 }
 
+resource "time_sleep" "kv_policy_propagation" {
+  depends_on      = [azurerm_key_vault_access_policy.pipeline]
+  create_duration = "30s"
+}
+
 resource "azurerm_key_vault_secret" "kv_example" {
   name         = local.key_vault_secret_name
   value        = "kv_example-value" # Replace with the actual secret value
   key_vault_id = azurerm_key_vault.kv_example.id
+  depends_on = [
+    time_sleep.kv_policy_propagation
+  ]
 }
 
 resource "azurerm_key_vault_access_policy" "apim" {
