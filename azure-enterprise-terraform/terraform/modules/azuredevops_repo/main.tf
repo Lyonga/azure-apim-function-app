e9a5fe4333ba69446de466061b1314b390461e43
clyonga <charles.lyonga1@accesscfa.com>
2026-02-25 09:56:45 -0500

    add provider

--- azure-enterprise-terraform/terraform/modules/azuredevops_repo/main.tf
+++ azure-enterprise-terraform/terraform/modules/azuredevops_repo/main.tf
@@ -1,6 +1,15 @@
 # This module is OFF by default to keep the project deployable even if you don't use Azure DevOps.
 # If enabled, configure the azuredevops provider in the stack's provider.tf.
 
+terraform {
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "~> 1.3"
+    }
+  }
+}
+
 data "azuredevops_project" "this" {
   count = var.enable ? 1 : 0
   name  = var.project_name
