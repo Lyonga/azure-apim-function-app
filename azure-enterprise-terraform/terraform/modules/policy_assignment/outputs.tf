4db2236689ca9eec341fe474fdda25e8e31cd359
clyonga <charles.lyonga1@accesscfa.com>
2026-02-27 13:26:22 -0500

    test

--- azure-enterprise-terraform/terraform/modules/policy_assignment/outputs.tf
+++ azure-enterprise-terraform/terraform/modules/policy_assignment/outputs.tf
@@ -1,2 +1,2 @@
-output "id" { value = azurerm_resource_policy_assignment.this.id }
-output "name" { value = azurerm_resource_policy_assignment.this.name }
+output "id" { value = azurerm_resource_group_policy_assignment.this.id }
+output "name" { value = azurerm_resource_group_policy_assignment.this.name }
