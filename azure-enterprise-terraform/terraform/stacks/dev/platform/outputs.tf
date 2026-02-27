68711bcab0ac36d304323fdb280d36f33a6c39b5
clyonga <charles.lyonga1@accesscfa.com>
2026-02-27 17:26:30 -0500

    fix output

--- azure-enterprise-terraform/terraform/stacks/dev/platform/outputs.tf
+++ azure-enterprise-terraform/terraform/stacks/dev/platform/outputs.tf
@@ -4,7 +4,7 @@ output "location" { value = var.location }
 output "vnet_name" { value = module.network.vnet_name }
 output "subnet_ids" { value = module.network.subnet_ids }
 
-output "keyvault_uri" { value = module.keyvault.vault_uri }
+//output "keyvault_uri" { value = module.keyvault.vault_uri }
 output "storage_account_name" { value = module.storage.account_name }
 output "acr_login_server" { value = module.acr.login_server }
 # output "aks_name" { value = module.aks.name }
