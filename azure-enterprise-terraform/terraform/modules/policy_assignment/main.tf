61b6cb37c47e559a2eb8a82623aaf5c6e83fc7ad
clyonga <charles.lyonga1@accesscfa.com>
2026-02-27 13:21:59 -0500

    wrong resource reference in policy assignment module

--- azure-enterprise-terraform/terraform/modules/policy_assignment/main.tf
+++ azure-enterprise-terraform/terraform/modules/policy_assignment/main.tf
@@ -1,12 +1,20 @@
-resource "azurerm_resource_policy_assignment" "this" {
+# resource "azurerm_resource_policy_assignment" "this" {
+#   name                 = var.name
+#   display_name         = var.display_name
+#   policy_definition_id = var.policy_definition_id
+
+#   # ✅ resource-group scope: pass RG ID here
+#   resource_id          = var.scope
+
+#   parameters = var.parameters == null ? null : jsonencode(var.parameters)
+
+#   metadata = jsonencode({ deployedBy = "terraform" })
+# }
+
+resource "azurerm_resource_group_policy_assignment" "this" {
   name                 = var.name
   display_name         = var.display_name
   policy_definition_id = var.policy_definition_id
-
-  # ✅ resource-group scope: pass RG ID here
-  resource_id          = var.scope
-
-  parameters = var.parameters == null ? null : jsonencode(var.parameters)
-
-  metadata = jsonencode({ deployedBy = "terraform" })
+  resource_group_id    = var.scope   # RG ID is valid here
+  parameters           = var.parameters == null ? null : jsonencode(var.parameters)
 }
