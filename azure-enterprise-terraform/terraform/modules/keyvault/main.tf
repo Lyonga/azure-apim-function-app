c868270b2106d39f68e3aaf5fc980d248e5d6e7d
clyonga <charles.lyonga1@accesscfa.com>
2026-03-02 14:29:53 -0500

    again

--- azure-enterprise-terraform/terraform/modules/keyvault/main.tf
+++ azure-enterprise-terraform/terraform/modules/keyvault/main.tf
@@ -1,16 +1,16 @@
-data "azurerm_client_config" "current" {
+# data "azurerm_client_config" "current" {
   
-}
-resource "azurerm_key_vault" "this" {
-  name                        = var.name
-  location                    = var.location
-  resource_group_name         = var.resource_group_name
-  #tenant_id                   = var.tenant_id
-  tenant_id = "${data.azurerm_client_config.current.tenant_id}"
-  sku_name                    = var.sku_name
-  purge_protection_enabled    = var.purge_protection_enabled
-  soft_delete_retention_days  = var.soft_delete_retention_days
-  enable_rbac_authorization   = var.enable_rbac_authorization
+# }
+# resource "azurerm_key_vault" "this" {
+#   name                        = var.name
+#   location                    = var.location
+#   resource_group_name         = var.resource_group_name
+#   #tenant_id                   = var.tenant_id
+#   tenant_id = "${data.azurerm_client_config.current.tenant_id}"
+#   sku_name                    = var.sku_name
+#   purge_protection_enabled    = var.purge_protection_enabled
+#   soft_delete_retention_days  = var.soft_delete_retention_days
+#   enable_rbac_authorization   = var.enable_rbac_authorization
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
--- azure-enterprise-terraform/terraform/modules/keyvault/variables.tf
+++ azure-enterprise-terraform/terraform/modules/keyvault/variables.tf
@@ -1,40 +1,40 @@
-variable "name" {
-  type = string
-}
+# variable "name" {
+#   type = string
+# }
 
-variable "resource_group_name" {
-  type = string
-}
+# variable "resource_group_name" {
+#   type = string
+# }
 
-variable "location" {
-  type = string
-}
+# variable "location" {
+#   type = string
+# }
 
-variable "sku_name" {
-  type    = string
-  default = "standard"
-}
+# variable "sku_name" {
+#   type    = string
+#   default = "standard"
+# }
 
-variable "purge_protection_enabled" {
-  type    = bool
-  default = true
-}
+# variable "purge_protection_enabled" {
+#   type    = bool
+#   default = true
+# }
 
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
+# variable "tags" {
+#   type    = map(string)
+#   default = {}
+# }
 
-# variable "tenant_id" { 
-#   type = string 
+# # variable "tenant_id" { 
+# #   type = string 
+# #   }
+
+# variable "enable_rbac_authorization" { 
+#   type = bool 
+#   default = true 
 #   }
 
-variable "enable_rbac_authorization" { 
-  type = bool 
-  default = true 
-  }
-
-variable "soft_delete_retention_days" { 
-  type = number 
-  default = 90 
-  }
+# variable "soft_delete_retention_days" { 
+#   type = number 
+#   default = 90 
+#   }
