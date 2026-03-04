be57a23f0c6d673d408effd2298f2701785d163f
clyonga <charles.lyonga1@accesscfa.com>
2026-03-04 16:23:12 -0500

    clean up

--- azure-enterprise-terraform/terraform/modules/keyvault/main.tf
+++ azure-enterprise-terraform/terraform/modules/keyvault/main.tf
@@ -11,7 +11,6 @@ resource "azurerm_key_vault" "this" {
   purge_protection_enabled    = var.purge_protection_enabled
   soft_delete_retention_days  = var.soft_delete_retention_days
   enable_rbac_authorization   = var.enable_rbac_authorization
-  # Network config: currently public
   public_network_access_enabled = true
   network_acls {
     bypass         = "AzureServices"
