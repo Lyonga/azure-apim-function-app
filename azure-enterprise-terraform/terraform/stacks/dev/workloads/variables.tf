8e91c05726e74587c4a080e41c2fff254711a299
clyonga <charles.lyonga1@accesscfa.com>
2026-03-06 17:44:40 -0500

    test

--- azure-enterprise-terraform/terraform/stacks/dev/workloads/variables.tf
+++ azure-enterprise-terraform/terraform/stacks/dev/workloads/variables.tf
@@ -2,21 +2,54 @@ variable "environment" {
   type    = string
   default = "dev"
 }
-variable "location" { type = string default = "eastus" }
-variable "project_name" { type = string default = "azure-enterprise-lab" }
-variable "owner" { type = string default = "app-team" }
-variable "cost_center" { type = string default = "cc-0001" }
+variable "location" { 
+  type = string 
+  default = "eastus" 
+  }
+variable "project_name" { 
+  type = string 
+  default = "azure-enterprise-lab" 
+  }
+variable "owner" { 
+  type = string 
+  default = "app-team" 
+  }
+variable "cost_center" { 
+  type = string 
+  default = "cc-0001" 
+  }
 
 # Reference platform remote state
-variable "platform_state_rg" { type = string default = "rg-tfstate-dev" }
-variable "platform_state_sa" { type = string default = "sttfstatedev001" }
-variable "platform_state_container" { type = string default = "tfstate" }
-variable "platform_state_key" { type = string default = "dev.platform.tfstate" }
+variable "platform_state_rg" { 
+  type = string 
+  default = "rg-tfstate-dev" 
+  }
+variable "platform_state_sa" { 
+  type = string 
+  default = "sttfstatedev001" 
+  }
+variable "platform_state_container" { 
+  type = string 
+  default = "tfstate" 
+  }
+variable "platform_state_key" { 
+  type = string 
+  default = "dev.platform.tfstate" 
+  }
 
 # Workload VM
-variable "create_demo_vm" { type = bool default = true }
-variable "vm_name" { type = string default = "vm-dev-demo-01" }
-variable "ssh_public_key" { type = string default = "ssh-rsa REPLACE_ME" }
+variable "create_demo_vm" { 
+  type = bool 
+  default = true 
+  }
+variable "vm_name" { 
+  type = string 
+  default = "vm-dev-demo-01" 
+  }
+variable "ssh_public_key" { 
+  type = string 
+  default = "ssh-rsa REPLACE_ME" 
+  }
 variable "vm_size" {
   description = "The size of the virtual machine."
   type        = string
@@ -34,7 +67,10 @@ variable "os_disk_size_gb" {
 }
 
 # Public LB demo
-variable "create_demo_lb" { type = bool default = true }
+variable "create_demo_lb" { 
+  type = bool 
+  default = true 
+  }
 variable "allocation_method" {
   description = "The allocation method for the public IP."
   type        = string
@@ -76,9 +112,15 @@ variable "analytics_sku" {
   default     = "PerGB2018"
 }
 
-variable "service_plan_sku"     { type = string  default = "Y1" }
-variable "runtime"              { type = string  default = "python" }
-variable "runtime_version"      { type = string  default = "3.11" }
-variable "collection_name"      { type = string  default = "default-collection" }
-variable "tags"            { type = map(string) default = {} }
-variable "kv_enable_rbac"  { type = bool default = false }
+variable "service_plan_sku"     { default = "Y1" }
+variable "runtime"              { default = "python" }
+variable "runtime_version"      { default = "3.11" }
+variable "collection_name"      { default = "default-collection" }
+variable "tags"            { 
+  type = map(string) 
+  default = {} 
+  }
+variable "kv_enable_rbac"  { 
+  type = bool 
+  default = false 
+  }
