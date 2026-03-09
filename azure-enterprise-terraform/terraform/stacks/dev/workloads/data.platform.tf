9269439d537763fc6031df1101f1607f4ccf40fd
Lyonga <60431189+Lyonga@users.noreply.github.com>
2026-03-09 17:29:35 -0400

    retry

--- azure-enterprise-terraform/terraform/stacks/dev/workloads/data.platform.tf
+++ azure-enterprise-terraform/terraform/stacks/dev/workloads/data.platform.tf
@@ -1,9 +1,10 @@
 data "terraform_remote_state" "platform" {
   backend = "azurerm"
+
   config = {
     resource_group_name  = "demo-test"
     storage_account_name = "demotest822e"
     container_name       = "deploy-container"
-    key                  = "platform.tfstate" 
+    key                  = "platform.tfstate"
   }
 }
