518d10cc416413aea462d584b55d4ce4f560a779
clyonga <charles.lyonga1@accesscfa.com>
2026-03-09 08:56:12 -0400

    remove duplicates

--- azure-enterprise-terraform/terraform/modules/apim/variables.tf
+++ azure-enterprise-terraform/terraform/modules/apim/variables.tf
@@ -17,8 +17,8 @@ variable "workload_rg_location" {
 }
 
 variable "name"                { type = string }
-variable "resource_group_name" { type = string }
-variable "location"            { type = string }
+# variable "resource_group_name" { type = string }
+# variable "location"            { type = string }
 variable "publisher_name"      { type = string }
 variable "publisher_email"     { type = string }
 variable "sku_name"            { 
